c69ea229406a26700b04b4d4151d24705b50c27c
lahi1998 <95356006+lahi1998@users.noreply.github.com>
2023-08-09 10:00:16 +0200

    Add files via upload

--- Sikker lagring  af  adgangskoder/Sikker lagring  af  adgangskoder/LogInd.cs
+++ Sikker lagring  af  adgangskoder/Sikker lagring  af  adgangskoder/LogInd.cs
@@ -14,64 +14,65 @@ namespace Sikker_lagring__af__adgangskoder
         {
             string? fetchedSalt, fetchedPassHash;
             bool LogindStatus = false;
-            
 
-            //Forbindelse der henter brugernavn, hashed kode og salt hvor username(brugernavn) matcher.
-            string connectionString = "server=localhost;user=Remote;password=Kode1234!;database=logindkryptering;";
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            try
             {
-                connection.Open();
-
-                string usernameToFetch = username; 
-                string sqlQuery = "SELECT PassHash, Salt FROM users WHERE Username = @username";
-
-                using (MySqlCommand command = new MySqlCommand(sqlQuery, connection))
+                //Forbindelse der henter brugernavn, hashed kode og salt hvor username(brugernavn) matcher.
+                string connectionString = "server=localhost;user=Remote;password=Kode1234!;database=logindkryptering;";
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
-                    command.Parameters.AddWithValue("@username", usernameToFetch);
+                    connection.Open();
 
-                    using (MySqlDataReader reader = command.ExecuteReader())
+                    string usernameToFetch = username;
+                    string sqlQuery = "SELECT PassHash, Salt FROM users WHERE Username = @username";
+
+                    using (MySqlCommand command = new MySqlCommand(sqlQuery, connection))
                     {
-                        // hvis der bliver fundet en bruger der matcher validres indtsatede kodeord
-                        // efter den er salted og hashed med den hashed kodeord fra databasen. + henter salt til hashing af det indtastede kodeord. 
-                        if (reader.Read())
+                        command.Parameters.AddWithValue("@username", usernameToFetch);
+
+                        using (MySqlDataReader reader = command.ExecuteReader())
                         {
-                            fetchedPassHash = reader["PassHash"].ToString();
-                            fetchedSalt = reader["Salt"].ToString();
-
-                            // salter det inputed kodeord fra login og matcher dem.
-                            Hasher hasher = new Hasher();
-                            byte[] salt = Encoding.ASCII.GetBytes(fetchedSalt);                            
-                            byte[] bytes = Encoding.ASCII.GetBytes(password);
-                            byte[] PS = new byte[bytes.Length + salt.Length];
-                            Buffer.BlockCopy(bytes, 0, PS, 0, bytes.Length);
-                            Buffer.BlockCopy(salt, 0, PS, bytes.Length, salt.Length);
-                            byte[] HashedPS = hasher.Hash(PS);
-
-
-                            Console.WriteLine(fetchedSalt);
-                            Console.WriteLine(BitConverter.ToString(salt).Replace("-", ""));
-                            Console.WriteLine(BitConverter.ToString(HashedPS).Replace("-", ""));
-                            Console.ReadKey();
-
-                            // converter det hashed og saltede kodeord til string så jeg kan matche dem.
-                            string saltedhash = BitConverter.ToString(HashedPS).Replace("-", "");
-                            
-                            // hvis de matcher ændre den logindstatus til true eller returnere vi senere bare false
-                            if (saltedhash == fetchedPassHash) 
+                            // hvis der bliver fundet en bruger der matcher validres indtsatede kodeord
+                            // efter den er salted og hashed med den hashed kodeord fra databasen. + henter salt til hashing af det indtastede kodeord. 
+                            if (reader.Read())
                             {
-                                LogindStatus = true;
-                            }
+                                fetchedPassHash = reader["PassHash"].ToString();
+                                fetchedSalt = reader["Salt"].ToString();
 
-                        }
-                        else
-                        {
-                            Console.WriteLine("Brugeren findes ikke test");
-                            Console.ReadKey();
+                                // salter det inputed kodeord fra login og matcher dem.
+                                Hasher hasher = new Hasher();
+                                byte[] salt = Convert.FromBase64String(fetchedSalt);
+                                byte[] bytes = Encoding.ASCII.GetBytes(password);
+                                byte[] PS = new byte[bytes.Length + salt.Length];
+                                Buffer.BlockCopy(bytes, 0, PS, 0, bytes.Length);
+                                Buffer.BlockCopy(salt, 0, PS, bytes.Length, salt.Length);
+                                byte[] HashedPS = hasher.Hash(PS);
+
+                                // converter det hashed og saltede kodeord til string så jeg kan matche dem.
+                                string saltedhash = Convert.ToBase64String(HashedPS).Replace("-", "");
+
+                                // hvis de matcher ændre den logindstatus til true eller returnere vi senere bare false
+                                if (saltedhash == fetchedPassHash)
+                                {
+                                    LogindStatus = true;
+                                }
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Brugeren findes ikke test");
+                                Console.ReadKey();
+                            }
                         }
                     }
-                }
 
-                connection.Close();
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine("An error occurred: " + ex.Message);
             }
 
 
